f27542a7a66fa85e9fdb8918df7771a047c6b91c
christian932 <christianfarnsworth@gmail.com>
2021-06-16 22:17:10 -0700

    alot of changes with mark

---
+++ water-my-plants/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav.js";
+
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import NewPlant from "./components/Plants";
+import PrivateRoute from "./components/PrivateRoute";
+import Welcome from "./components/Welcome";
+
+const Content = styled.div`
+  margin-top: 150px;
+  @media (min-width: 700px) {
+    margin-top: 100px;
+  }
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Content>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/profile" component={UserProfile} />
+        <PrivateRoute path="/home" component={Home} />
+        <PrivateRoute path="/plants" component={NewPlant} />
+      </Content>
+    </div>
+  );
+}
+
+export default App;
