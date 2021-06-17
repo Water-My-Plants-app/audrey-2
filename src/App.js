e508e4e896871225501ea37236b454c50e8d9a24
rockyFierro <finnslighthouse@gmail.com>
2021-06-16 21:32:03 -0700

    base api for testing: https://www.dnd5eapi.co/api/ ?

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
 function App() {
-    axios.post('https://water-my-plants-node.herokuapp.com/',
-        {
-            username: 'testUser',
-            password: 'password',
-        })
+    axios.get('https://www.dnd5eapi.co/api/monsters')
         .then(response => console.log(response))
         .catch(error => console.log(error))
 
-  return (
-    <div className="App">
-        <h1>hello there...</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>hello there...</h1>
+        </div>
+    );
 }
 
 export default App;
