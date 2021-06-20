64fed3d1d39d900895b78159184e2fa103b4c5ab
Fry-Fr <ryjlwdg89@gmail.com>
2021-06-20 01:07:05 -0400

    improved validation ux design

--- src/validation/signUpFormSchema.js
+++ src/validation/signUpFormSchema.js
@@ -3,7 +3,9 @@ import * as yup from 'yup';
 export default yup.object().shape({
     fullname: yup.string().required("Required Field."),
     username: yup.string().required("Need a Username."),
-    phonenumber: yup.number().required().positive().integer().typeError("phone number required."),
+    phonenumber: yup.string().required("Need valid number").matches(/^[0-9]+$/, "Must be only digits")
+    .min(10,"valid 10 digit number.")
+    .max(10,"valid 10 digit number."),
     password: yup.string().required("Password Required."),
     password2: yup.string().required("Password2 Required."),
     termsOfService: yup.boolean().oneOf([true], "Accept Terms of Service.")
