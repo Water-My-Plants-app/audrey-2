447ad7dee18caf66bcb0a7607ef19dbe40165593
Fry-Fr <ryjlwdg89@gmail.com>
2021-06-20 00:07:39 -0400

    phonenumber field has unique typeError

--- src/validation/signUpFormSchema.js
+++ src/validation/signUpFormSchema.js
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 export default yup.object().shape({
     fullname: yup.string().required("Required Field."),
-    username: yup.string().required("Need a Username"),
-    phonenumber: yup.number().required().positive().integer(),
+    username: yup.string().required("Need a Username."),
+    phonenumber: yup.number().required().positive().integer().typeError("phone number required."),
     password: yup.string().required("Password Required."),
     password2: yup.string().required("Password2 Required."),
     termsOfService: yup.boolean().oneOf([true], "Accept Terms of Service.")
