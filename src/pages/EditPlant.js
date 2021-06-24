2a1dc9d4f0c8dc1944ecef28bebb36186739052d
rockyFierro <finnslighthouse@gmail.com>
2021-06-23 20:29:22 -0700

    ready to merge

--- src/pages/EditPlant.js
+++ src/pages/EditPlant.js
@@ -1,31 +1,84 @@
-import React, {useEffect, useState} from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState } from 'react';
 
+const EditPlant = (props) => {
+    const { plants } = props;
+    const [edit, setEdit] = useState(
+        plants.map( item => {
+            return{
+                id: item.id,
+                nickname: item.nickname,
+                species: item.species,
+                h2oFrequency: item.h2oFrequency,
+                image: item.image
+            }
+        })
+    );
 
-const EditPlant = () => {
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        const id = parseInt(event.target.id);
 
-    const [ plantInfo, setPlantInfo ] = useState({title:'',image:'',schedule:'',species:'',id:''});
-    const history = useHistory();
+        setEdit(
+            edit.map( item => {
+                if (item.id === id){
+                    return {...item,[name]: value}
+                }else {
+                    return item
+                }
+            })
+        )
 
-    useEffect(() => {
-            setPlantInfo(history.location.state.plant)
-    }, []);
+    }
 
+    const handleSave = (event) => {
+        event.preventDefault();
+        console.log("saved", edit)
+    }
 
     return (
-
         <>
-            <form>
-                <h2>Edit {plantInfo.title}</h2>
-                <label htmlFor="title">Name:
+           {plants ?
+            plants.map( (plant,i) => {
+                return (
+                <form key={i} onSubmit={handleSave} >
+                <h2>Edit {plant.nickname}</h2>
+                <label htmlFor="nickname">Name:</label>
                     <input
+                        id={plant.id}
                         type="text"
-                        name="title"
-                        value={plantInfo.title}
-
+                        name="nickname"
+                        value={edit[i].nickname}
+                        onChange={handleChange}
                     />
-                </label>
-            </form>
+                <label htmlFor="species">Species:</label>
+                    <input
+                        id={plant.id}
+                        type="text"
+                        name="species"
+                        value={edit[i].species}
+                        onChange={handleChange}
+                    />
+                <label htmlFor="h2oFrequency">Schedule:</label>
+                    <input
+                        id={plant.id}
+                        type="text-field"
+                        name="h2oFrequency"
+                        value={edit[i].h2oFrequency}
+                        onChange={handleChange}
+                    />
+                <label htmlFor="image">Image:</label>
+                    <input
+                        id={plant.id}
+                        type="text"
+                        name="image"
+                        value={edit[i].image}
+                        onChange={handleChange}
+                    />
+            </form>)
+            }) : undefined}
+
+            <button onClick={handleSave}>save</button>
         </>
 
     )
--- src/utilities/axiosCalls.js
+++ src/utilities/axiosCalls.js
@@ -1,11 +1,23 @@
+// import axios from 'axios';
+//
+// export const axiosWithAuth =() => {
+//     const token = localStorage.getItem('token');
+//
+//     return axios.create({
+//         headers: {
+//             Authorization: token,
+//         },
+//     });
+// };
+
 import axios from 'axios';
 
-export const axiosWithAuth =() => {
-    const token = localStorage.getItem('token');
-
+export const axiosWithAuth = () =>{
+    const token = localStorage.getItem('token')
     return axios.create({
+        baseURL:'https://buildweekplants.herokuapp.com',
         headers: {
-            Authorization: token,
-        },
+            Authorization: token
+        }
     });
 };
