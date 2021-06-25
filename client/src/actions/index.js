40bbf57cf4589d93b71a2bc807f0f1fd0a789a65
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-06-24 21:28:17 -0700

    added action types > starting on action creators.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,20 @@ export const GET_PLANTS = "GET_PLANTS";
 export const GET_SUCCESS = "GET_SUCCESS";
 export const GET_ERROR = "GET_ERROR";
 
+export const POST_REGISTER = "POST_REGISTER";
+export const POST_LOGIN = "POST_LOGIN";
+export const POST_PLANTS = "POST_PLANTS";
+export const POST_SUCCESS = "POST_SUCCESS";
+export const POST_ERROR = "POST_ERROR";
+
+export const PUT_PLANT = "PUT_PLANT";
+export const PUT_ERROR = "PUT_ERROR";
+export const PUT_SUCCESS = "PUT_SUCCESS";
+
+export const DELETE_PLANT = "DELETE_PLANT";
+export const DELETE_ERROR = "DELETE_ERROR";
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+
 export const getPlants = ()=> (dispatch)=>{
     dispatch({type: GET_PLANTS})
     axiosWithAuth()
