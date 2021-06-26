e937ecfdac1c00cce084c6c77a0492afd12a1b51
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-06-26 01:18:00 -0400

    Loading if hanging after post login; renders wrong credential error message above login form.

---
+++ client/src/pages/LoadingPage.js
@@ -0,0 +1,17 @@
+const LoadingPage = () => {
+    return(
+        <>
+        <img alt="loading"
+        src="https://cdn.dribbble.com/users/107759/screenshots/2436386/copper-loader.gif"
+        style={{
+            'display': 'flex',
+            'alignItems': 'center',
+            'justifyContent': 'center',
+            'margin': '15rem auto 0'             
+        }}
+         />
+        </>
+    )
+};
+
+export default LoadingPage;
