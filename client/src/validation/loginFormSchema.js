17414c5fcd74eb41910150a066a91bd99b092edf
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-06-27 15:51:28 -0700

    ready to merge

--- client/src/validation/loginFormSchema.js
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export default yup.object().shape({
-    username: yup.string().required("Please enter a username"),
-    password: yup.string().required("Please enter a password")
-})
