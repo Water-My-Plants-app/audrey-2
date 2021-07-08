dd519b2b9108ab9f630407f19235cf203a2f79b6
Ryan <68477278+Fry-Fr@users.noreply.github.com>
2021-07-08 18:22:07 -0400

    Update AddPlant.js

--- src/pages/AddPlant.js
+++ src/pages/AddPlant.js
@@ -93,7 +93,7 @@ export default function UserProfile() {
             species: addPlantFormValues.species,
         })
         .then(res => {
-            push("/")
+            push("/home")
             window.location.reload()
         })
         .catch(err => {
